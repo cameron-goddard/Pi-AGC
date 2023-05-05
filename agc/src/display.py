37f79201b0664a87294d6aee3cb479abbf55f182
Cameron Goddard <camerongoddard@me.com>
2023-05-05 19:32:28 -0400

    display structure

---
+++ agc/src/display.py
@@ -0,0 +1,18 @@
+
+
+class Display:
+
+    def __init__(self):
+        pass
+
+    def update_row(self, val, ):
+        pass
+
+    def update_verb(self, val):
+        pass
+
+    def update_noun(self, val):
+        pass
+
+    def update_prog(self, val):
+        pass
