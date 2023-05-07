81733d0be60ac37f8d0f60009a4c23f2b50bed23
Nissi Ragland <nkr33@cornell.edu>
2023-05-07 00:22:14 -0400

    Started display

--- agc/src/display.py
+++ agc/src/display.py
@@ -1,11 +1,47 @@
-
+import pygame
+import math
 
 class Display:
 
     def __init__(self):
-        pass
+        pygame.init()
+        
+        # Constants
+        self.screen_size = (240, 400)
+        self.rect_height = self.screen_size[1]/5
+        self.text_height = math.ceil((2/3)*self.rect_height)
+        self.bar_height = self.rect_height - self.text_height
+        
+        self.screen = pygame.display.set_mode((self.screen_size[0], self.screen_size[1]))
+        self.r0 = pygame.Rect(0, self.screen_size[1]-(self.rect_height*1), self.screen_size[0], self.rect_height)
+        self.r1 = pygame.Rect(0, self.screen_size[1]-(self.rect_height*2), self.screen_size[0], self.rect_height)
+        self.r2 = pygame.Rect(0, self.screen_size[1]-(self.rect_height*3), self.screen_size[0], self.rect_height)
+        self.r3 = pygame.Rect(0, self.screen_size[1]-(self.rect_height*4), self.screen_size[0], self.rect_height)
+        self.r4 = pygame.Rect(0, self.screen_size[1]-(self.rect_height*5), self.screen_size[0], self.rect_height)
+        
 
-    def update_row(self, val, ):
+        
+        code_run = True
+        
+        while code_run:
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    code_run = False
+            
+            self.screen.fill("black")
+            pygame.draw.rect(self.screen, "black", self.r0)
+            pygame.draw.rect(self.screen, "black", self.r1)
+            pygame.draw.rect(self.screen, "black", self.r2)
+            pygame.draw.rect(self.screen, "black", self.r3)
+            pygame.draw.rect(self.screen, "black", self.r4)
+            
+            
+            pygame.display.flip()
+        
+        pygame.quit()
+        
+
+    def update_row(self, val, index):
         pass
 
     def update_verb(self, val):
@@ -19,3 +55,5 @@ class Display:
 
     def clear_all(self, excluding=None):
         pass
+    
+d = Display()
