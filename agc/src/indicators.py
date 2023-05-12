7408479e8fee771278b1dbc3c8d4d9bc2356db68
Nissi Ragland <nkr33@cornell.edu>
2023-05-12 05:33:49 -0400

    pins

--- agc/src/indicators.py
+++ agc/src/indicators.py
@@ -1,5 +1,6 @@
 
-
+import RPi.GPIO as GPIO
+import pygame
 
 class Indicators:
     """
@@ -11,3 +12,38 @@ class Indicators:
 
     def clear_all(self) -> None:
         pass
+    
+    """
+    key - gpio
+    
+    noun - 14
+    verb - 20
+    0 - 11
+    - - 15
+    + - 21
+    1 - 0
+    4 - 18
+    7 - 9
+    2 - 5
+    5 - 23
+    8 - 10
+    3 - 6
+    6 - 24
+    9 - 22
+    key release - 13
+    prog - 17
+    clear - 27
+    reset - 19
+    enter - 26
+    
+    unused gpios:
+    2
+    3
+    4
+    7
+    8
+    10
+    12
+    16
+    18
+"""
